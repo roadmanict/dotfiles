e00a28848605fd5ac10bb6ce1355f05aa7f3e3d4
Geert Weggemans <geert@Geerts-MacBook-Pro.local>
2023-07-28 09:10:05 +0200

    Add git left / right commands

--- nvim/lua/roadmanict/remap.lua
+++ nvim/lua/roadmanict/remap.lua
@@ -40,3 +40,6 @@ vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Reinstall packages?
 vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+
+vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
+vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
