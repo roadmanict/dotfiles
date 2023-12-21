b9dedb367b60741c24f80be28e856b7e47285bd4
Geert Weggemans <geert.weggemans.cic@nl.ibm.com>
2023-12-21 13:16:27 +0100

    Add conform

--- nvim/lua/roadmanict/remap.lua
+++ nvim/lua/roadmanict/remap.lua
@@ -27,9 +27,6 @@ vim.keymap.set({ "n", "v" }, "<leader>d", [["_d]])
 vim.keymap.set("n", "Q", "<nop>")
 vim.keymap.set("n", "<C-f>", "<cmd>silent !tmux neww tmux-sessionizer<CR>")
 
--- Format current buffer
-vim.keymap.set("n", "<leader>f", vim.lsp.buf.format)
-
 vim.keymap.set("n", "<C-k>", "<cmd>cnext<CR>zz")
 vim.keymap.set("n", "<C-j>", "<cmd>cprev<CR>zz")
 vim.keymap.set("n", "<leader>k", "<cmd>lnext<CR>zz")
