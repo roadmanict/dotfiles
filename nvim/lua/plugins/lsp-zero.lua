4adc3e2527c1f1c1b66e917e49da577accd9b848
Geert Weggemans <geert@geerts-mbp.groningen.nl.ibm.com>
2024-01-09 15:43:44 +0100

    Fix hover

--- nvim/lua/plugins/lsp-zero.lua
+++ nvim/lua/plugins/lsp-zero.lua
@@ -32,7 +32,7 @@ return {
                     local opts = { buffer = ev.buf, remap = false }
 
                     vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
-                    vim.keymap.set("n", "k", function() vim.lsp.buf.hover() end, opts)
+                    vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
                     vim.keymap.set("n", "<leader>vws", function() vim.lsp.buf.workspace_symbol() end, opts)
                     vim.keymap.set("n", "<leader>vd", function() vim.diagnostic.open_float() end, opts)
                     --    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
