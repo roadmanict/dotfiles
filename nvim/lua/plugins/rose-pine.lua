11957310cb2edb9b7201c21df5066301fb304c2b
Geert Weggemans <git@relaylay.nl>
2024-04-10 14:11:18 +0200

    Format lua file

--- nvim/lua/plugins/rose-pine.lua
+++ nvim/lua/plugins/rose-pine.lua
@@ -1,12 +1,12 @@
 return {
-    {
-        'rose-pine/neovim',
-        name = 'rose-pine',
-        lazy = false,
-        priority = 1000,
-        config = function()
-            vim.opt.termguicolors = true
-            vim.cmd.colorscheme('rose-pine-moon')
-        end
-    },
+	{
+		"rose-pine/neovim",
+		name = "rose-pine",
+		lazy = false,
+		priority = 1000,
+		config = function()
+			vim.opt.termguicolors = true
+			vim.cmd.colorscheme("rose-pine-moon")
+		end,
+	},
 }
