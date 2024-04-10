6de06ac155b38ef15e77295de19967d394d48093
Geert Weggemans <git@relaylay.nl>
2024-04-10 14:55:49 +0200

    Fix treesitter

--- nvim/lua/plugins/treesitter.lua
+++ nvim/lua/plugins/treesitter.lua
@@ -1,33 +1,35 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        build = ":TSUpdate",
-        ---@type TSConfig
-        ---@diagnostic disable-next-line: missing-fields
-        opts = {
-            -- A list of parser names, or "all" (the four listed parsers should always be installed)
-            ensure_installed = {
-                "c",
-                "lua",
-                "vim",
-                "vimdoc",
-                "bash",
-            },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		---@type TSConfig
+		---@diagnostic disable-next-line: missing-fields
+		opts = {
+			-- A list of parser names, or "all" (the four listed parsers should always be installed)
+			ensure_installed = {
+				"c",
+				"lua",
+				"vim",
+				"vimdoc",
+				"bash",
+			},
 
-            -- Install parsers synchronously (only applied to `ensure_installed`)
-            sync_install = false,
+			-- Install parsers synchronously (only applied to `ensure_installed`)
+			sync_install = false,
 
-            -- Automatically install missing parsers when entering buffer
-            -- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
-            auto_install = true,
+			-- Automatically install missing parsers when entering buffer
+			-- Recommendation: set to false if you don't have `tree-sitter` CLI installed locally
+			auto_install = true,
 
-            highlight = {
-                -- `false` will disable the whole extension
-                enable = true,
-                additional_vim_regex_highlighting = false,
-            },
-        },
-
-    },
-    { "nvim-treesitter/nvim-treesitter-context" },
+			highlight = {
+				-- `false` will disable the whole extension
+				enable = true,
+				additional_vim_regex_highlighting = false,
+			},
+		},
+		config = function(_, opts)
+			require("nvim-treesitter.configs").setup(opts)
+		end,
+	},
+	{ "nvim-treesitter/nvim-treesitter-context" },
 }
