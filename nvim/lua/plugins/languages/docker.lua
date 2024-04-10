6ae41cfe86e7338183812b483c5a47c9a29d7dfa
Geert Weggemans <git@relaylay.nl>
2024-04-10 13:50:32 +0200

    Enable dockerls

---
+++ nvim/lua/plugins/languages/docker.lua
@@ -0,0 +1,22 @@
+return {
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "dockerfile" })
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "dockerls" })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = { dockerls = {} },
+		},
+	},
+}
