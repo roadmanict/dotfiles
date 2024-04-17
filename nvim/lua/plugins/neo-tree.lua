86f9570f3a39ff27d1ec2ea4777f9766dd845309
Geert Weggemans <git@relaylay.nl>
2024-04-17 09:44:17 +0200

    neo tree style

--- nvim/lua/plugins/neo-tree.lua
+++ nvim/lua/plugins/neo-tree.lua
@@ -1,29 +1,34 @@
 return {
-    {
-        "nvim-neo-tree/neo-tree.nvim",
-        branch = "v3.x",
-        dependencies = {
-            "nvim-lua/plenary.nvim",
-            "nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
-            "MunifTanjim/nui.nvim",
-            -- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
-        },
-        keys = {
-            { "<leader>pv", "<cmd>Neotree filesystem focus left<CR>", desc = "NeoTree" },
-        },
-        opts = {
-            window = {
-                mappings = {
-                    ["%"] = {
-                        "add",
-                        -- some commands may take optional config options, see `:h neo-tree-mappings` for details
-                        config = {
-                            show_path = "none", -- "none", "relative", "absolute"
-                        }
-                    },
-                    ["d"] = { "add_directory" }
-                }
-            }
-        }
-    }
+	{
+		"nvim-neo-tree/neo-tree.nvim",
+		branch = "v3.x",
+		dependencies = {
+			"nvim-lua/plenary.nvim",
+			"nvim-tree/nvim-web-devicons", -- not strictly required, but recommended
+			"MunifTanjim/nui.nvim",
+			-- "3rd/image.nvim", -- Optional image support in preview window: See `# Preview Mode` for more information
+		},
+		keys = {
+			{ "<leader>pv", "<cmd>Neotree filesystem focus left<CR>", desc = "NeoTree" },
+		},
+		opts = {
+			filesystem = {
+				filtered_items = {
+					visible = true,
+				},
+			},
+			window = {
+				mappings = {
+					["%"] = {
+						"add",
+						-- some commands may take optional config options, see `:h neo-tree-mappings` for details
+						config = {
+							show_path = "none", -- "none", "relative", "absolute"
+						},
+					},
+					["d"] = { "add_directory" },
+				},
+			},
+		},
+	},
 }
