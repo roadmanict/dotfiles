03a10abd6448f88c669a1a96111545e14f8a5308
Geert Weggemans <git@relaylay.nl>
2024-04-29 14:31:34 +0200

    Add rustup to init.sh

--- init.sh
+++ init.sh
@@ -52,6 +52,8 @@ if [ "$system" = "Linux" ]; then
 
         /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"
 
+        curl --proto '=https' --tlsv1.2 -sSf https://sh.rustup.rs | sh -s -- -y
+
         exit
 EOF
 fi
