6fa6ba3204f77140e4816eb20a193ea28677d5fc
Geert Weggemans <git@relaylay.nl>
2024-05-21 10:10:53 +0200

    Improve nvim completions

--- nvim/.config/nvim/lua/plugins/completions.lua
+++ nvim/.config/nvim/lua/plugins/completions.lua
@@ -1,51 +1,55 @@
 return {
-    {
-        'hrsh7th/nvim-cmp',
-        dependencies = {
-            { 'hrsh7th/cmp-nvim-lsp' },
-            {
-                'L3MON4D3/LuaSnip',
-                dependencies = {
-                    "saadparwaiz1/cmp_luasnip",
-                    "rafamadriz/friendly-snippets",
-                },
-            },
-        },
-        config = function()
-            local cmp = require('cmp')
+	{
+		"hrsh7th/nvim-cmp",
+		dependencies = {
+			{ "hrsh7th/cmp-nvim-lsp" },
+			{
+				"L3MON4D3/LuaSnip",
+				dependencies = {
+					"saadparwaiz1/cmp_luasnip",
+					"rafamadriz/friendly-snippets",
+				},
+			},
+			-- This tiny plugin adds vscode-like pictograms to neovim built-in lsp
+			{ "onsails/lspkind.nvim" },
+		},
+		config = function()
+			local cmp = require("cmp")
 
-            require('luasnip.loaders.from_vscode').lazy_load()
+			require("luasnip.loaders.from_vscode").lazy_load()
 
-            vim.opt.completeopt = { 'menu', 'menuone', 'noselect' }
+			local lspkind = require("lspkind")
+			lspkind.init({})
 
-            cmp.setup({
-                snippet = {
-                    expand = function(args)
-                        require("luasnip").lsp_expand(args.body)
-                    end,
-                },
-                window = {
-                    completion = cmp.config.window.bordered(),
-                    documentation = cmp.config.window.bordered(),
-                },
-                preselect = 'item',
-                completion = {
-                    completeopt = 'menu,menuone,noinsert'
-                },
-                sources = {
-                    { name = 'path' },
-                    { name = 'nvim_lsp' },
-                    { name = 'nvim_lua' },
-                    { name = 'buffer',  keyword_length = 3 },
-                    { name = 'luasnip', keyword_length = 2 },
-                },
-                mapping = {
-                    ['<C-p>'] = cmp.mapping.select_prev_item(),
-                    ['<C-n>'] = cmp.mapping.select_next_item(),
-                    ['<C-y>'] = cmp.mapping.confirm({ select = true }),
-                    ["<C-Space>"] = cmp.mapping.complete(),
-                },
-            })
-        end
-    },
+			vim.opt.completeopt = { "menu", "menuone", "noselect" }
+
+			cmp.setup({
+				snippet = {
+					expand = function(args)
+						require("luasnip").lsp_expand(args.body)
+					end,
+				},
+				window = {
+					completion = cmp.config.window.bordered(),
+					documentation = cmp.config.window.bordered(),
+				},
+				preselect = "item",
+				completion = {
+					completeopt = "menu,menuone,noinsert",
+				},
+				sources = {
+					{ name = "nvim_lsp" },
+					{ name = "path" },
+					{ name = "buffer", keyword_length = 3 },
+					{ name = "nvim_lua" },
+					{ name = "luasnip", keyword_length = 2 },
+				},
+				mapping = {
+					["<C-p>"] = cmp.mapping.select_prev_item(),
+					["<C-n>"] = cmp.mapping.select_next_item(),
+					["<C-y>"] = cmp.mapping.confirm({ select = true }),
+				},
+			})
+		end,
+	},
 }
--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -66,25 +66,15 @@ return {
 					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
 					local opts = { buffer = ev.buf, remap = false }
 
+					vim.lsp.inlay_hint.enable()
+
 					vim.keymap.set("n", "gd", function()
 						vim.lsp.buf.definition()
-					end, opts)
-					vim.keymap.set("n", "K", function()
-						vim.lsp.buf.hover()
 					end, opts)
 					vim.keymap.set("n", "<leader>vws", function()
 						vim.lsp.buf.workspace_symbol()
 					end, opts)
-					vim.keymap.set("n", "<leader>vd", function()
-						vim.diagnostic.open_float()
-					end, opts)
 					--    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
-					vim.keymap.set("n", "]d", function()
-						vim.diagnostic.goto_next()
-					end, opts)
-					vim.keymap.set("n", "[d", function()
-						vim.diagnostic.goto_prev()
-					end, opts)
 					vim.keymap.set("n", "<leader>vca", function()
 						vim.lsp.buf.code_action()
 					end, opts)
