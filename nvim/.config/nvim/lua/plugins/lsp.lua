1ed3d768e48b382741ca568547549955fea2231e
Geert Weggemans <git@relaylay.nl>
2024-06-05 08:56:10 +0200

    Refactor on attach lsp

--- nvim/.config/nvim/lua/plugins/lsp.lua
+++ nvim/.config/nvim/lua/plugins/lsp.lua
@@ -47,48 +47,44 @@ return {
 
 			local lspconfig = require("lspconfig")
 
+			local on_attach = function(client, bufnr)
+				client.server_capabilities.semanticTokensProvider = nil
+
+				-- enable completion triggered by <c-x><c-o>
+				vim.bo[bufnr].omnifunc = "v:lua.vim.lsp.omnifunc"
+				local keymap_opts = { buffer = bufnr, remap = false }
+
+				vim.lsp.inlay_hint.enable()
+
+				vim.keymap.set("n", "gd", function()
+					vim.lsp.buf.definition()
+				end, keymap_opts)
+				vim.keymap.set("n", "<leader>vws", function()
+					vim.lsp.buf.workspace_symbol()
+				end, keymap_opts)
+				--    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
+				vim.keymap.set("n", "<leader>vca", function()
+					vim.lsp.buf.code_action()
+				end, keymap_opts)
+				vim.keymap.set("n", "<leader>vrr", function()
+					vim.lsp.buf.references()
+				end, keymap_opts)
+				vim.keymap.set("n", "<leader>vrn", function()
+					vim.lsp.buf.rename()
+				end, keymap_opts)
+				vim.keymap.set("i", "<C-h>", function()
+					vim.lsp.buf.signature_help()
+				end, keymap_opts)
+			end
+
 			for server, server_opts in pairs(opts.servers) do
 				server_opts = vim.tbl_deep_extend("force", {
 					capabilities = vim.deepcopy(capabilities),
+					on_attach = on_attach,
 				}, server_opts or {})
 
 				lspconfig[server].setup(server_opts)
 			end
-
-			vim.api.nvim_create_autocmd("lspattach", {
-				group = vim.api.nvim_create_augroup("userlspconfig", {}),
-				callback = function(ev)
-					-- Disable lsp highlighting in favor of treesitter
-					local client = vim.lsp.get_client_by_id(ev.data.client_id)
-					client.server_capabilities.semanticTokensProvider = nil
-
-					-- enable completion triggered by <c-x><c-o>
-					vim.bo[ev.buf].omnifunc = "v:lua.vim.lsp.omnifunc"
-					local opts = { buffer = ev.buf, remap = false }
-
-					vim.lsp.inlay_hint.enable()
-
-					vim.keymap.set("n", "gd", function()
-						vim.lsp.buf.definition()
-					end, opts)
-					vim.keymap.set("n", "<leader>vws", function()
-						vim.lsp.buf.workspace_symbol()
-					end, opts)
-					--    vim.keymap.set("n", "<leader>vf", function() vim.lsp.buf.format() end, opts)
-					vim.keymap.set("n", "<leader>vca", function()
-						vim.lsp.buf.code_action()
-					end, opts)
-					vim.keymap.set("n", "<leader>vrr", function()
-						vim.lsp.buf.references()
-					end, opts)
-					vim.keymap.set("n", "<leader>vrn", function()
-						vim.lsp.buf.rename()
-					end, opts)
-					vim.keymap.set("i", "<C-h>", function()
-						vim.lsp.buf.signature_help()
-					end, opts)
-				end,
-			})
 		end,
 	},
 }
