5f6dd1a633c43d0abe0e9ea7b5c28f92f2bbc5fd
Geert Weggemans <git@relaylay.nl>
2024-07-16 13:22:56 +0200

    Remove neo-tree

--- nvim/.config/nvim/lua/roadmanict/remap.lua
+++ nvim/.config/nvim/lua/roadmanict/remap.lua
@@ -36,7 +36,7 @@ vim.keymap.set("n", "<leader>s", [[:%s/\<<C-r><C-w>\>/<C-r><C-w>/gI<Left><Left><
 vim.keymap.set("n", "<leader>x", "<cmd>!chmod +x %<CR>", { silent = true })
 
 -- Reinstall packages?
-vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>");
+vim.keymap.set("n", "<leader>vpp", "<cmd>e ~/.dotfiles/nvim/.config/nvim/lua/theprimeagen/packer.lua<CR>")
 
 vim.keymap.set("n", "gf", "<cmd>diffget //2<CR>")
 vim.keymap.set("n", "gj", "<cmd>diffget //3<CR>")
