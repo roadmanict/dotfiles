f950bbab651176aa2b0512b77198e37aba2dad6e
Geert Weggemans <git@relaylay.nl>
2024-09-12 10:06:52 +0200

    Rename tsserver

--- nvim/.config/nvim/lua/plugins/languages/angular.lua
+++ nvim/.config/nvim/lua/plugins/languages/angular.lua
@@ -17,14 +17,14 @@ return {
 		"williamboman/mason-lspconfig.nvim",
 		opts = function(_, opts)
 			opts.ensure_installed = opts.ensure_installed or {}
-			vim.list_extend(opts.ensure_installed, { "tsserver", "eslint", "angularls" })
+			vim.list_extend(opts.ensure_installed, { "ts_ls", "eslint", "angularls" })
 		end,
 	},
 	{
 		"neovim/nvim-lspconfig",
 		opts = {
 			servers = {
-				tsserver = {},
+				ts_ls = {},
 				eslint = {},
 				angularls = {},
 			},
--- nvim/.config/nvim/lua/plugins/languages/typescript.lua
+++ nvim/.config/nvim/lua/plugins/languages/typescript.lua
@@ -1,32 +1,32 @@
 return {
-    {
-        "nvim-treesitter/nvim-treesitter",
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { "typescript", "javascript" })
-        end,
-    },
-    {
-        'WhoIsSethDaniel/mason-tool-installer.nvim',
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { "prettierd" })
-        end,
-    },
-    {
-        "williamboman/mason-lspconfig.nvim",
-        opts = function(_, opts)
-            opts.ensure_installed = opts.ensure_installed or {}
-            vim.list_extend(opts.ensure_installed, { "tsserver", "eslint" })
-        end,
-    },
-    {
-        'neovim/nvim-lspconfig',
-        opts = {
-            servers = {
-                tsserver = {},
-                eslint = {}
-            }
-        },
-    },
+	{
+		"nvim-treesitter/nvim-treesitter",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "typescript", "javascript" })
+		end,
+	},
+	{
+		"WhoIsSethDaniel/mason-tool-installer.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "prettierd" })
+		end,
+	},
+	{
+		"williamboman/mason-lspconfig.nvim",
+		opts = function(_, opts)
+			opts.ensure_installed = opts.ensure_installed or {}
+			vim.list_extend(opts.ensure_installed, { "ts_ls", "eslint" })
+		end,
+	},
+	{
+		"neovim/nvim-lspconfig",
+		opts = {
+			servers = {
+				ts_ls = {},
+				eslint = {},
+			},
+		},
+	},
 }
