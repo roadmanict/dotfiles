efa9fb562e363c74dccd33b48a602304b9de5d4b
Geert Weggemans <git@relaylay.nl>
2025-07-01 11:55:05 +0200

    Add nvim lspconfigs for nvim-scm

--- nvim-scm/.config/nvim-scm/init.lua
+++ nvim-scm/.config/nvim-scm/init.lua
@@ -2,3 +2,10 @@ vim.g.mapleader = " "
 
 require("roadmanict")
 require("plugins")
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+    automatic_enable = false
+})
+
+vim.lsp.enable({"lua_ls"})
