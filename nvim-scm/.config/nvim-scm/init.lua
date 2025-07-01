83353a2b6a0dd20ef611bca9f72404ba97e604a7
Geert Weggemans <git@relaylay.nl>
2025-07-01 14:39:42 +0200

    Add treesitter and telescope to nvim-scm

--- nvim-scm/.config/nvim-scm/init.lua
+++ nvim-scm/.config/nvim-scm/init.lua
@@ -3,9 +3,12 @@ vim.g.mapleader = " "
 require("roadmanict")
 require("plugins")
 
+require("nvim-treesitter").setup()
+require("nvim-treesitter").install({'typescript', 'javascript', 'rust', 'java', 'html'})
+require('telescope').setup()
 require("mason").setup()
 require("mason-lspconfig").setup({
     automatic_enable = false
 })
 
-vim.lsp.enable({"lua_ls"})
+vim.lsp.enable({"lua_ls", "ts_ls", "angularls"})
