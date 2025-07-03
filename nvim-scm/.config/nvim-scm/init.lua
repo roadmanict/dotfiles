cc268fb1c5ca90ae16dec636bf925c9add29fd9b
Geert Weggemans <git@relaylay.nl>
2025-07-03 09:37:19 +0200

    Update init.lua

--- nvim-scm/.config/nvim-scm/init.lua
+++ nvim-scm/.config/nvim-scm/init.lua
@@ -4,11 +4,26 @@ require("roadmanict")
 require("plugins")
 
 require("nvim-treesitter").setup()
-require("nvim-treesitter").install({'typescript', 'javascript', 'rust', 'java', 'html'})
-require('telescope').setup()
-require("mason").setup()
-require("mason-lspconfig").setup({
-    automatic_enable = false
+require("nvim-treesitter").install({ "typescript", "javascript", "rust", "java", "html" }):wait(300000)
+require("telescope").setup()
+
+require("conform").setup({
+	formatters_by_ft = {
+		lua = { "stylua" },
+		typescript = { "prettierd" },
+	},
+	format_on_save = {
+		-- These options will be passed to conform.format()
+		timeout_ms = 500,
+		lsp_format = "fallback",
+	},
 })
 
-vim.lsp.enable({"lua_ls", "ts_ls", "angularls"})
+require("blink.cmp").setup()
+
+require("mason").setup()
+require("mason-lspconfig").setup({
+	automatic_enable = false,
+})
+
+vim.lsp.enable({ "lua_ls", "ts_ls", "angularls" })
